a7c31c90e711c00367d99588dd8e302c58a5b704
ShadyFox2004 <amboudreau.amb@gmail.com>
2021-05-27 10:35:55 -0400

    Update FuelInjector.lua

--- PHOENIXV2_SuperComputer/FuelInjector.lua
+++ PHOENIXV2_SuperComputer/FuelInjector.lua
@@ -1,7 +1,7 @@
 --[[
 	AUTHOR: shadyfox
 	TITLE: PHONIX v2 Fuel Injector Module
-	DESCRIPTION: Inject send the fuel injection to the motor if recives "inject
+	DESCRIPTION: Inject the fuel to the motor if recives "inject"
 	NOTE: This code is unix standard, it does one thing and does it well. It is ment to be use in conjonction with other litle codes like this in the PHOENIX super computer
 --]]
 
